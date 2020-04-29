4c05449981a485fe66cca6c8624e513849107c1f
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-29 00:40:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  
+  get '/reversename/:name' do
+    params[:name].reverse 
+  end
+
+  get '/square/:number' do
+    (params[:number].to_i**2).to_s
+  end
+  
+  get '/say/:number/:phrase' do
+    times_said = ''
+    params[:number].to_i.each do
+      times_said += params[:phrase]
+    end
+    times_said
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  end
+  
+  get '/:operation/:number1/:number2' do 
+  end 
 
 end
