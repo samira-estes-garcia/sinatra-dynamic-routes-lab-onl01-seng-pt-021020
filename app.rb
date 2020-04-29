fe2451a2c0111584f63dbe32b7aa64aab26184d0
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-29 00:53:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,8 +23,8 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do 
-    number1 = params[number1].to_i
-    number2 = params[number2].to_i
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
     answer = ''
     
     case params[:operation]
